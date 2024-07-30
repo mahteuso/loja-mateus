e53e1eec31a2e5e6a0c3b34a7b5d764c85a2c1b2
mahteuso <mateus@ufscar.br>
2024-07-30 09:33:15 -0300

    Starting the project with .gitignore and pom.xml

---
+++ src/main/java/com/loja_mateus/ClietesCadastradosApplication.java
@@ -0,0 +1,13 @@
+package com.loja_mateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClietesCadastradosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClietesCadastradosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loja_mateus/ClietesCadastradosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja_mateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClietesCadastradosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
